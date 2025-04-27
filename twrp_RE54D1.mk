467aba073b2e3707a9855c8be07b0488ae48e449
Yoshk4e <105790589+Yoshk4e@users.noreply.github.com>
2025-04-27 03:09:17 +0300

    Update twrp_RE54D1.mk

--- twrp_RE54D1.mk
+++ twrp_RE54D1.mk
@@ -5,9 +5,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Enable virtual A/B OTA
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Enable virtual A/B OTA
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
